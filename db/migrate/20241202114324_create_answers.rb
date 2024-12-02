d4fce84812eef3a797637c29a46fbe28f93ab859
Aleksey Moseev <a12345678@Alekseis-MacBook-Pro.local>
2024-12-02 14:50:52 +0300

    feat: default project model generated

---
+++ db/migrate/20241202114324_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :answers do |t|
+      t.string :body
+      t.references :question_id, null: false, foreign_key: true
+      t.references :user_id, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
