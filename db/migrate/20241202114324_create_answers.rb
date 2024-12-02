9283b71599444c83cc52b8f88cb85dab6f9e469d
Aleksey Moseev <a12345678@Alekseis-MacBook-Pro.local>
2024-12-02 15:17:31 +0300

    fix: fixes for references

--- db/migrate/20241202114324_create_answers.rb
+++ db/migrate/20241202114324_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
     create_table :answers do |t|
       t.string :body
       t.references :question_id, null: false, foreign_key: true
-      t.references :user_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
