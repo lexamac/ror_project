1060bf2a9737b59220e402ca8632f52a72c421ef
Aleksey Moseev <a12345678@Alekseis-MacBook-Pro.local>
2024-12-02 15:32:37 +0300

    fix: fixes for answer

--- db/migrate/20241202114324_create_answers.rb
+++ db/migrate/20241202114324_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.string :body
+      t.boolean :correct
       t.references :question, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
